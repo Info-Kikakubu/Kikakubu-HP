dd15f16aae6dcf21208d484417b1ab86fc20d6d6
Info-Kikakubu <info.datascience.daisaikikaku@gmail.com>
2026-09-24 15:00:07 +0900

    Update links.js

--- content/links.js
+++ content/links.js
@@ -6,16 +6,19 @@ window.festivalContent.links = {
     {
       "platform": "Twitter(現: X)",
       "account": "千葉大学情報・データサイエンス学部2026大学祭広報課",
+      "link": "https://x.com/chibaudsfes2025"
       "icon": "./assets/X-logo-black.png"
     },
     {
       "platform": "Instagram",
       "account": "千葉大学情報・データサイエンス学部/大祭企画",
+      "link": "https://www.instagram.com/datascience.daisaikikaku/"
       "icon": "./assets/Instagram_Glyph_Gradient.png"
     },
     {
       "platform": "YouTube",
       "account": "千葉大学情報・データサイエンス学部",
+      "link": "https://www.youtube.com/@%E5%8D%83%E8%91%89%E5%A4%A7%E5%AD%A6%E6%83%85%E5%A0%B1DS%E5%AD%A6%E9%83%A8"
       "icon": "./assets/youtube_icon_red_digital.png"
     }
   ]
