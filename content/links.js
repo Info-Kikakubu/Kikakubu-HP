ced89fb588e328f069e22a6e8a74256f0a801230
Info-Kikakubu <info.datascience.daisaikikaku@gmail.com>
2026-09-24 14:52:52 +0900

    Update links.js

--- content/links.js
+++ content/links.js
@@ -6,19 +6,16 @@ window.festivalContent.links = {
     {
       "platform": "Twitter(現: X)",
       "account": "千葉大学情報・データサイエンス学部2026大学祭広報課",
-      "label": "デー祭公式Xはこちら",
       "icon": "./assets/X-logo-black.png"
     },
     {
       "platform": "Instagram",
       "account": "千葉大学情報・データサイエンス学部/大祭企画",
-      "label": "デー祭公式Instagramはこちら",
       "icon": "./assets/Instagram_Glyph_Gradient.png"
     },
     {
       "platform": "YouTube",
       "account": "千葉大学情報・データサイエンス学部",
-      "label": "情報・データサイエンス学部公式チャンネルはこちら",
       "icon": "./assets/youtube_icon_red_digital.png"
     }
   ]
